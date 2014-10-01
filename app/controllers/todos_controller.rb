ccb0ca1aef84e844213022a4acd37d8b2e638764
Colt Steele <coltsteele1@gmail.com>
2014-10-01 14:46:14 -0700

    initial commit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,17 +7,22 @@ class TodosController < ApplicationController
 		end
 	end
 
-	def search
-		params
-	end
-
 	def new
 		@todo = Todo.new
+	end
+
+	def update
+		@todo = Todo.find(params[:id])
+		@todo.completed = params[:todo][:completed]
+		if @todo.save
+			render json: @todo
+		end
 	end
 
 	def create
 		# binding.pry
 		@todo = Todo.new(todo_params)
+		@todo.completed = false
 		respond_to do |format|
 			if @todo.save
 				format.html{redirect_to todos_path}
@@ -25,10 +30,17 @@ class TodosController < ApplicationController
 			else
 				format.html{render :new}
 				format.json{render json: @todo.errors, status: :unprocessable_entity}
+			end
 		end
-		end
+	end
 
-		
+  def destroy
+	  @todo = Todo.find(params[:id])
+	  if @todo.destroy
+	    render json: {}
+	  else
+	    render status: 400, nothing: true
+	  end
 	end
 
 	private
